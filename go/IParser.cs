f9b4155e54e7e6261a478a8586af63ddd88f7568
Ibrahim Sammour <s1mm0ur@outlook.com>
2016-04-01 22:55:54 +0300

    +IParser

---
+++ go/IParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace go
+{
+    interface IParser
+    {
+        bool ArgsValid();
+        bool NameExists();
+        void Add(string name,string path);
+        void List();
+        void Update(string name,string path);
+        void Delete(string name);
+        void Rename(string oldName, string newName);
+    }
+}
--- go/Program.cs
+++ go/Program.cs
@@ -10,6 +10,7 @@ namespace go
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
